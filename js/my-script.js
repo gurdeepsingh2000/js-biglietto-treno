0d94ed54ee17f24b2d352fffbc22e72b58f9c4c5
gurdeepsingh2000 <gurdeepsingh20000329@gmail.com>
2021-05-28 15:40:57 +0200

    update

---
+++ js/my-script.js
@@ -0,0 +1,7 @@
+var number_km = prompt('Inserisci quanti km vuoi percorrere');
+var number_age = prompt('Inserisci la tua età');
+var ticket_price_km = parseInt(0,21);
+var x = ticket_price_km * 20 / 100;
+var y = parseInt(40)
+
+document.getElementById ('sconto').innerHTML = x ;
