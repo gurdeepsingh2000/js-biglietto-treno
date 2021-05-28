9c5a42ab74520fdd55903480dba5e9f7fd1d60ca
gurdeepsingh2000 <gurdeepsingh20000329@gmail.com>
2021-05-28 17:19:10 +0200

    esercizio finito

--- js/my-script.js
+++ js/my-script.js
@@ -1,17 +1,21 @@
 var number_km = prompt('Inserisci quanti km vuoi percorrere');
 var number_age = prompt('Inserisci la tua età');
+var ticket_price = 0.21 * parseInt(number_km) ;
 var x = ticket_price * 20 / 100;
 var y = ticket_price * 40 / 100;
-var ticket_price = 0.21
+var deal_20_price = ticket_price - x
+var deal_40_price = ticket_price - y
+document.getElementById ('sconto').innerHTML = ticket_price;
+
 
 if (number_age < 18) {
-    document.getElementById ('sconto').innerHTML = 'Questo è il prezzo scontato' + number_km * ticket_price - x ;
+    document.getElementById ('sconto').innerHTML = 'Questo è il prezzo scontato' + deal_price_20;
 }
 
 else if (number_age > 65) {
-    document.getElementById ('sconto').innerHTML = 'Questo è il prezzo scontato' + ((number_km * ticket_price) - ( number_km * y)) ;
+    document.getElementById ('sconto').innerHTML = 'Questo è il prezzo scontato' + deal_40_price;
 }
 
-else if ( number_age => 18  && number_age <= 65) {
-    document.getElementById ('sconto').innerHTML = number_km * ticket_price ;
+else if (number_age >= 18 && number_age <= 65 ) {
+    document.getElementById ('sconto').innerHTML = 'Prezzo non scontato ' + ticket_price;
 }
